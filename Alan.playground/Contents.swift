1915af280b2e416c9b99eb9476810d909740a88a
aliozanoguz <ali.oguz@hisarschool.k12.tr>
2024-10-22 08:26:52 +0300

    Alan Hesapla

---
+++ Alan.playground/Contents.swift
@@ -0,0 +1,10 @@
+func dikdörtgenalan(a: Int, b: Int) -> Int {
+    let genişlik = a
+    let uzunluk = b
+    let alan = genişlik * uzunluk
+    return alan
+}
+
+let sonuc = dikdörtgenalan(a: 10, b: 12)
+
+print(sonuc)
